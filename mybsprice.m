502612fa29f0289546e44cf83bca17687e0d0f33
Cici Wei <chiimin@163.com>
2016-12-01 10:33:17 -0800

    Delete mybsprice.m

--- mybsprice.m
+++
@@ -1,7 +0,0 @@
-% define a function which calculates the BS price
-function [price] = mybsprice(S0, K, r, T, sigma) 
-    d1 = (log(S0./K) + (r+sigma.^2/2).*T)./(sigma.*(T.^(1/2)));
-    d2 = (log(S0./K) + (r-sigma.^2/2).*T)./(sigma.*(T.^(1/2)));
-
-    price = S0 .* normcdf(d1) - exp(-r.*T) .* K .* normcdf(d2);
-end
